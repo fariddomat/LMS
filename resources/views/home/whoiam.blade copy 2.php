a11b61a5ea2f36ffe9c3ba21a469485517b71be0
Farid Domat <fariddomat.000@gmail.com>
2023-08-25 12:35:07 +0300

    update

---
+++ resources/views/home/whoiam.blade copy 2.php
@@ -0,0 +1,8 @@
+@extends('home.layouts._app')
+
+@section('content')
+    <!-- Start main-content -->
+    <div class="main-content-area">
+        <h2 class="mt-200 mb-200 pt-50 pb-50" style="text-align: center; font-size: 64px; color:#375651">الصفحة تحت الصيانة</h2>
+    </div>
+@endsection
